88ff4fa0aff03f53d3f42592dfc2ede70f5a5064
Gaurangi97 <nimashaumaya64@gmail.com>
2024-03-16 11:25:16 +0530

    data function

--- __main__.py
+++ __main__.py
@@ -1,1 +1,9 @@
-println('Hello world');
+def createDataBase():
+    try:
+        print('DataBase created successfully')
+    except Exception as e:
+        print('Error:', e)
+
+
+if __name__ == '__main__':
+    createDataBase()
